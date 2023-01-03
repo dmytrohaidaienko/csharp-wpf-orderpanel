7708506501895b7d778579f317aa30821486d0f4
dmytrohaidaienko <dmytrohaidaienko@gmail.com>
2023-01-03 21:50:23 +0200

    Created authorization window.

---
+++ csharp-wpf-orderpanel/Views/AuthorizationWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace csharp_wpf_orderpanel.Views
+{
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void authorizationLoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void authorizationCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
